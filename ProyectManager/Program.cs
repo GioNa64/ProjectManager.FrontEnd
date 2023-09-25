a1f1896b385805a7d40c66e13ee06e7467fc6314
Manuel Armando Flores Villalta <manuelvillalta819@gmail.com>
2023-09-25 10:55:16 -0600

    Se creo y se codifico las vistas de Colaborador

--- ProyectManager/Program.cs
+++ ProyectManager/Program.cs
@@ -8,9 +8,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44353/api/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7294/api/") });
  
 builder.Services.AddScoped<IProyectoService, ProyectoService>();
+builder.Services.AddScoped<IColaboradorService, ColaboradorService>();
 
 builder.Services.AddMudServices(); //Importación de los servicios
 await builder.Build().RunAsync();
