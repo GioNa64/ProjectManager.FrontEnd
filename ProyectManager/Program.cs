7e42075ec12d6d83b3bf1339bce0b6e55bbb25e7
Giovanni Najera <najera.giovanni64@gmail.com>
2023-09-25 09:03:42 -0600

    Creacion de estructura de FrontEnd

---
+++ ProyectManager/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using ProyectManager;
+using ProyectManager.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44353/api/") });
+ 
+builder.Services.AddScoped<IProyectoService, ProyectoService>();
+
+builder.Services.AddMudServices(); //Importación de los servicios
+await builder.Build().RunAsync();
